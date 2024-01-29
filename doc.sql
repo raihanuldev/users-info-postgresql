f9a4a713f19258548795578cb5cc2acdcbf18707
UniqueCoderrihan <rihanthedev@gmail.com>
2024-01-29 20:54:52 +0600

    added doc

---
+++ doc.sql
@@ -0,0 +1,14 @@
+-- Create database ; write it on CMd
+
+CREATE DATABASE usersDB;
+
+-- create a Table
+CREATE TABLE book (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(20),
+    role VARCHAR(20),
+    description VARCHAR(250)
+);
+
+-- POst on database
+
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const PORT = 1000;
 
-
+app.use(express.json());
 
 
 app.listen(PORT,()=>{
